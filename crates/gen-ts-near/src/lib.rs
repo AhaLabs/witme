615dac3e81ffab0f4151fffe6dc9e60aac82f42a
Willem Wyndham <wyndham@cs.unc.edu>
2022-03-11 15:43:01 -0500

    fix: export primitive types and clean up empty args

--- crates/gen-ts-near/src/lib.rs
+++ crates/gen-ts-near/src/lib.rs
@@ -234,12 +234,16 @@ impl Ts {
     }
 
     fn print_args(&mut self, iface: &Interface, func: &Function, param_start: usize) {
-        self.src.ts("(args: {\n");
-
-        self.print_args_type(iface, func, param_start);
-        self.src.ts("}");
+        self.src.ts("(args");
         if func.params.is_empty() {
-            self.src.ts(" = {}");
+          self.src.ts(" = {}");
+        } else {
+          self.src.ts(": {\n");
+          self.print_args_type(iface, func, param_start);
+          self.src.ts("}");
+        }
+        if func.params.is_empty() {
+            
         };
         self.src.ts(", options?: ");
         self.src.ts(if is_change(func) {
@@ -387,68 +391,68 @@ impl Generator for Ts {
  * @maximum 18446744073709551615
  * @asType integer
  */
-type u64 = number;
+export type u64 = number;
 /** 
  * @minimum -9223372036854775808
  * @maximum 9223372036854775807
  * @asType integer
  */
-type i64 = number;
+export type i64 = number;
 
 /**
 * @minimum  0 
 * @maximum 255
 * @asType integer
 * */
-type u8 = number;
+export type u8 = number;
 /**
  * @minimum  -128 
  * @maximum 127
  * @asType integer
  * */
-  type i8 = number;
+export type i8 = number;
 /**
  * @minimum  0 
  * @maximum 65535
  * @asType integer
  * */
-  type u16 = number;
+export type u16 = number;
 /**
  * @minimum -32768 
  * @maximum 32767
  * @asType integer
  * */
-  type i16 = number;
+export type i16 = number;
 /**
  * @minimum 0 
  * @maximum 4294967295
  * @asType integer
  * */
-  type u32 = number;
+export type u32 = number;
   /**
  * @minimum 0 
  * @maximum 4294967295
  * @asType integer
  * */
-  type usize = number;
+export type usize = number;
 /**
  * @minimum  -2147483648 
  * @maximum 2147483647
  * @asType integer
  * */
-  type i32 = number;
+export type i32 = number;
 
 /**
  * @minimum -3.40282347E+38
  * @maximum 3.40282347E+38
  */
-type f32 = number;
+export type f32 = number;
 
 /**
  * @minimum -1.7976931348623157E+308
  * @maximum 1.7976931348623157E+308
  */
-type f64 = number;
+export type f64 = number;
 ");
     }
 
