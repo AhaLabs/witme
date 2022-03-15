4f9d635bb3fdb9179fc87314cc195ef7d1888142
Willem Wyndham <wyndham@cs.unc.edu>
2022-03-15 19:37:09 +0100

    fix: embed sdk files in `src` (#10)

--- src/embeded.rs
+++ src/embeded.rs
@@ -1,2 +1,2 @@
-pub const SDK: &str = include_str!("../crates/near-sdk/sdk.wit");
-pub const STANDARDS: &str = include_str!("../crates/near-sdk/standards.wit");
+pub const SDK: &str = include_str!("./near-sdk/sdk.wit");
+pub const STANDARDS: &str = include_str!("./near-sdk/standards.wit");
