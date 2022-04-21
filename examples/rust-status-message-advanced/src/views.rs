fd6d7b425be46c9f94d233990adb049c5163b665
Willem Wyndham <wyndham@cs.unc.edu>
2022-04-21 10:26:39 -0400

    feat(NEAR): add inject command for injecting data into custom section

--- examples/rust-status-message-advanced/src/views.rs
+++ examples/rust-status-message-advanced/src/views.rs
@@ -1,15 +1,14 @@
-use near_sdk::near_bindgen;
 use crate::*;
+use near_sdk::near_bindgen;
 
 trait ViewMessage {
-  fn get_message(self) -> Message;
+    fn get_message(self) -> Message;
 }
 
 #[near_bindgen]
 impl ViewMessage for Contract {
     /// A view call to get the current message
     fn get_message(self) -> Message {
-      self.message
-  }
-
+        self.message
+    }
 }
